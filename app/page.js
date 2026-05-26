38fceb09cd236d5f2ad046aa338d34cb7ef67864
ivansalasalejandro19-dev <ivansalasalejandro19@gmail.com>
2026-05-26 17:55:20 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -869,29 +869,33 @@ const roster = {
           {openTeam === team ? 'Volver' : 'Abrir'}
         </button>
 
-      {/* LOGO */}
+   {/* LOGO */}
 <div className="mb-4 flex justify-center">
-  <div
-    className="
-      p-[3px]
-      rounded-2xl
-      bg-gradient-to-br
-      from-pink-500
-      via-purple-500
-      to-cyan-400
-      shadow-lg shadow-pink-500/30
-      transition-all duration-300
-      hover:scale-105
-      hover:shadow-cyan-400/40
-    "
-  >
-    <div className="bg-[#14081f] rounded-2xl p-2">
+  <div className="relative w-28 h-28">
+    
+    {/* BORDE */}
+    <div
+      className="
+        absolute inset-0
+        rounded-2xl
+        bg-gradient-to-br
+        from-pink-500
+        via-purple-500
+        to-cyan-400
+        blur-[2px]
+        opacity-80
+      "
+    />
+
+    {/* IMAGEN */}
+    <div className="relative bg-[#14081f] rounded-2xl p-1">
       <img
         src={teamLogos[team]}
         alt={team}
         className="w-28 h-28 object-contain rounded-xl"
       />
     </div>
+
   </div>
 </div>
         {/* NOMBRE */}
