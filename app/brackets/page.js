82efd3ad86b929ec5149f5da4cc9366529d9fad2
ivansalasalejandro19-dev <ivansalasalejandro19@gmail.com>
2026-05-31 16:29:46 -0300

    Update page.js

--- app/brackets/page.js
+++ app/brackets/page.js
@@ -7,7 +7,7 @@ import {
   updateDoc,
   increment
 } from "firebase/firestore"
-import { db } from "../lib/firebase"
+import { db } from "./lib/firebase"
 import { motion } from "framer-motion"
 import {
   Play,
