2afd496e1bae2ddc978ff80070d08a295f618b8c
ivansalasalejandro19-dev <ivansalasalejandro19@gmail.com>
2026-05-31 04:16:06 -0300

    Update route.js

--- app/api/auth/discord/route.js
+++ app/api/auth/discord/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 export async function GET() {
   const CLIENT_ID = process.env.DISCORD_CLIENT_ID
 
-  const redirectUri = "http://localhost:3000/api/auth/discord/callback"
+  const redirectUri = "http://lotus-rift.vercel.app/api/auth/discord/callback"
 
   const url = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
     redirectUri
