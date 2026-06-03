d1cd2f9739de34dda09796ce7f0118f5f7905d21
ivansalasalejandro19-dev <ivansalasalejandro19@gmail.com>
2026-06-02 23:12:35 -0300

    moec

--- app/pickem/page.js
+++ app/pickem/page.js
@@ -7,15 +7,21 @@ import Leaderboard from './components/Leaderboard'
 
 export default function PickEmPage() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#09030f] via-[#14081f] to-[#050108] text-white">
+    <div className="min-h-screen relative overflow-hidden text-white bg-gradient-to-b from-[#09030f] via-[#14081f] to-[#050108]">
 
-      <Hero />
+      {/* 🌌 Glow background (AQUÍ VA EL EFECTO NUEVO) */}
+      <div className="absolute inset-0 pointer-events-none">
+        <div className="absolute -top-40 left-1/2 -translate-x-1/2 w-[600px] h-[600px] bg-purple-600/20 blur-[120px]" />
+        <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-cyan-500/10 blur-[120px]" />
+      </div>
 
-      <CrystalBall />
-
-      <LotusTracker />
-
-      <Leaderboard />
+      {/* 🧠 Contenido encima del fondo */}
+      <div className="relative z-10">
+        <Hero />
+        <CrystalBall />
+        <LotusTracker />
+        <Leaderboard />
+      </div>
 
     </div>
   )
