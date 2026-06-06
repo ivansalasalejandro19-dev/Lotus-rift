d1636e11161fa70e2a778c6f683bca9ce2edf73e
ivansalasalejandro19-dev <ivansalasalejandro19@gmail.com>
2026-06-06 17:21:26 -0300

    Update page.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { useAuth } from '../../context/AuthContext'
+import { useAuth } from '../context/AuthContext'
 
 export default function AdminPage() {
 
